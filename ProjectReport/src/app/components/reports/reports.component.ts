a663d5ac5fb1d398cd916aa829c07e61f9ca41d0
Izhan Khan <izhankhan001@gmail.com>
2022-11-30 19:22:22 +0530

    30-11-2022 commit

--- ProjectReport/src/app/components/reports/reports.component.ts
+++ ProjectReport/src/app/components/reports/reports.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-reports',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // generatePDF(){
+  //   let pdf = new jsPDF()
+
+  //   pdf.html(this.el.nativeElement,{
+  //     callback:(pdf) => {
+  //       pdf.save("output.pdf")
+  //     }
+  //   })
+  // }
+
+  download(){
+    var element = document.getElementById('report')!
+
+    html2canvas(element).then((canvas)=>{
+
+      console.log(canvas);
+
+      var imgData = canvas.toDataURL('image/png');
+
+      var doc = new jsPDF();
+
+      var imgHeight = canvas.height *208/canvas.width;
+
+      doc.addImage(imgData,0,0,208,imgHeight);
+
+      doc.save("report.xls");
+ 
+    })
+
+  }
+  
 
 }
